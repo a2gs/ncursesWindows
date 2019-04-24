9f8d1a8d18ca3a7aa2680a51dc103a995eda1ce4
Andre Augusto Giannotti Scota <andre.scota@gmail.com>
2019-04-24 01:32:23 -0300

    c.c Window control

--- c.c
+++ c.c
@@ -22,6 +22,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
 #include <ncurses.h>
 
 
@@ -51,31 +52,46 @@
 
 /* -------------------------------------------------------------------------------------------------------- */
 
+void signalHandle(int sig)
+{
+	switch(sig){
+		case SIGWINCH:
+			break;
+		default:
+			break;
+	}
+}
+
 /* int main(int argc, char *argv[])
  *
- *
- *
  * INPUT:
- * OUTPUT:
+ * OUTPUT (to SO):
  */
 int main(int argc, char *argv[])
 {
+#define FIELD1_SZ 50
+#define FIELD2_SZ 30
+
 	int cursor = 0;
 	int  cKey = 0;
-	WINDOW *mainWindow = NULL;
-	WINDOW *board[3] = {NULL};
+	int lineCenter = 0, colCenter = 0;
+	int win0_maxx = 0, win0_maxy = 0;
 	char fmt_STATUS_BAR_1[FMT_STATUS_BAR_1_SZ +1] = {0};
+	char *msg = NULL;
+	char field1[FIELD1_SZ] = {'\0'}, field2[FIELD2_SZ] = {'\0'};
+	WINDOW *board[3] = {NULL};
 
-	mainWindow = initscr();
-	keypad(stdscr, TRUE);
-	cbreak();
-	noecho();
-	nonl();
-
-	if(mainWindow == NULL){
+	if(initscr() == NULL){;
 		printf("Erro initializating ncurses!\n");
 		return(-1);
 	}
+	keypad(stdscr, TRUE);
+	cbreak();
+	noecho();
+
+	/* https://invisible-island.net/ncurses/ncurses-intro.html */
+	/* signal(SIGWINCH, signalHandle); */
+	/* TODO: configurar o SCREEN RESIZE para nao ser recebido como user input: KEY_RESIZE */
 
 	if((LINES < 24) || (COLS < 80)){
 		endwin();
@@ -107,42 +123,89 @@ int main(int argc, char *argv[])
 	cursor = curs_set(0);
 
 	do{
-		board[0] = newwin(3, 5, 2, 1);
-		board[1] = newwin(3, 15, 6, 1);
-		board[2] = newwin(4, 30, 10, 1);
+		int keyResize = 0;
+		lineCenter = LINES/2;
+		colCenter = COLS/2;
 
 		clear();
 
-		mvprintw(0,0, "[%d]", cKey);
+		snprintf(fmt_STATUS_BAR_1, FMT_STATUS_BAR_1_SZ, STATUS_BAR_1, "localhost:666", "Unknow");
+		mvwprintw(stdscr, STATUS_BAR_1_LINE_N, STATUS_BAR_1_COL_N(fmt_STATUS_BAR_1), fmt_STATUS_BAR_1);
+		mvwprintw(stdscr, STATUS_BAR_2_LINE_N, STATUS_BAR_2_COL_N, STATUS_BAR_2);
+		refresh();
+		wrefresh(stdscr);
+
+		board[0] = newwin(10+10, 40+40, lineCenter-10, colCenter-40);
+		win0_maxx = getmaxx(board[0]);
+		win0_maxy = getmaxy(board[0]);
+		/*
+		board[1] = newwin(3, 15, 6, 1);
+		board[2] = newwin(4, 30, 10, 1);
+		*/
 
 		wattron(board[0], A_BOLD);
 
 		box(board[0], 0, 0);
-		box(board[1], 0, 0);
-		wborder(board[2], '1', '2', '3', '4', '5', '6', '7', '8');
-
-		mvwaddch(board[0], 1, 2, 'a');
-		mvwaddch(board[1], 1, 2, 'b');
-		mvwprintw(board[2], 1, 2, "%s", "abcdef");
-
 
 		wattroff(board[0], A_BOLD);
 
-		snprintf(fmt_STATUS_BAR_1, FMT_STATUS_BAR_1_SZ, STATUS_BAR_1, "localhost:666", "Unknow");
-		mvprintw(STATUS_BAR_1_LINE_N, STATUS_BAR_1_COL_N(fmt_STATUS_BAR_1), fmt_STATUS_BAR_1);
-		mvprintw(STATUS_BAR_2_LINE_N, STATUS_BAR_2_COL_N, STATUS_BAR_2);
+		/*
+		box(board[1], 0, 0);
+		wborder(board[2], '1', '2', '3', '4', '5', '6', '7', '8');
+		*/
+		msg = "Title 1";
+		wattron(board[0], A_UNDERLINE);
+		mvwprintw(board[0], 1, ((win0_maxx-strlen(msg))/2), msg);
+		wattroff(board[0], A_UNDERLINE);
+
+		mvwprintw(board[0], 4, 2, "Field 1:");
+		mvwprintw(board[0], 5, 2, "Field 2:");
+
+		echo();
+		curs_set(2);
 
 		refresh();
-
 		wrefresh(board[0]);
+
+		keyResize = !KEY_RESIZE;
+		do{
+			keyResize = mvwgetnstr(board[0], 4, 2+sizeof("Field 1:"), field1, FIELD1_SZ);
+		}while(keyResize == KEY_RESIZE);
+
+		keyResize = !KEY_RESIZE;
+		do{
+			keyResize = mvwgetnstr(board[0], 5, 2+sizeof("Field 2:"), field2, FIELD2_SZ);
+		}while(keyResize == KEY_RESIZE);
+
+		/*
+		mvwaddch(board[1], 1, 2, 'b');
+		mvwprintw(board[2], 1, 2, "%s", "abcdef");
+		*/
+
+		mvwprintw(board[0], 8, 2, "Enter 1: [%s]", field1);
+		mvwprintw(board[0], 9, 2, "Enter 2: [%s]", field2);
+
+
+		mvwprintw(board[0], 12, 2, "[ESC] to exit");
+
+		curs_set(0);
+
+		refresh();
+		wrefresh(board[0]);
+
+		/*
 		wrefresh(board[1]);
 		wrefresh(board[2]);
+		*/
+
 
 		cKey = getch();
 
 		delwin(board[0]);
+		/*
 		delwin(board[1]);
 		delwin(board[2]);
+		*/
 
 	}while(cKey != ESC_KEY);
 
--- makefile
+++ makefile
@@ -1,6 +1,7 @@
-all:
+all: clean
 	cc -o a a.c -lncurses -Wall
 	cc -o b b.c -lncurses -Wall
+	cc -o c c.c -lncurses -Wall -Wno-unused-but-set-variable
 
 clean:
-	rm -rf a b
+	rm -rf a b c
